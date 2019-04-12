4099b5bf600121d390a8bc8e4af0f79269f6da93
VPaulger <vaughn_paulger@live.ca>
2019-04-12 10:48:13 -0400

    feat: annual-reports-page.php

---
+++ wp-content/themes/PPTTheme/page-annual-reports.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main annual-reports" role="main">
+
+      <header>
+        <div class="banner">
+          <h1>annual reports</h1>
+        </div>
+      </header>
+      <section>
+        <h3>be part of something bigger than yourself </h3>
+        <p>We believe that everyone should have the right to make informed choices about their sexual health.  We’ve been working tirelessly for more that 50 years to protect and promote that right - because, for all of the strides we’ve made, sexual and reporductive health rights continue to be undermined.  Now, you have a choice to make. Join our ovement to unapologetically revolutionize the sexual health dialogue.</p>
+      </section>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>

b462706715485a7e47d2e4550d44fbc9d2a186a9
VPaulger <vaughn_paulger@live.ca>
2019-04-16 11:34:18 -0400

    feat: annual reports page with dummy data and iframe

---
+++ wp-content/themes/PPTTheme/page-annual-reports.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main annual-reports" role="main">
+
+      <header class="banner">
+        <h1>annual reports</h1>
+      </header>
+      <section>
+
+        <h3>current edition</h3>
+        <iframe src="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" width="100%" height="500px" scrolling="no"></iframe>
+
+        <h4>download</h4>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto Annual Report 2017-2018</p>
+        </a>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto Annual Report 2016-2017</p>
+        </a>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto Annual Report 2015-2016</p>
+        </a>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto Annual Report 2014-2015</p>
+        </a>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto Annual Report 2013-2014</p>
+        </a>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto Annual Report 2012-2013</p>
+        </a>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto Annual Report 2011-2012</p>
+        </a>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto Annual Report 2010-2011</p>
+        </a>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto Annual Report 2009-2010</p>
+        </a>
+
+        <h3 class="strategic-h3">strategic plan</h3>
+        <h4>download</h4>
+        <a href="http://localhost/PlannedParenthood/wp-content/uploads/2019/04/Vaughn-Paulger-Resume-2019.pdf" download>
+          <img src=" <?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
+          <p>Planned Parenthood Toronto 2015-2020 Strategic Plan</p>
+        </a>
+      </section>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/PPTTheme/page-partnerships.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main partnerships" role="main">
+
+      <header class="banner">
+        <h1>seeking funding partners</h1>
+      </header>
+
+      <section>
+        <h3>Corporate Funders: Invest Strategically in PPT</h3>
+        <p>
+          As a corporate funder, you have philanthropic goals and priorities to meet your strategic branding, 
+          imaging and visibility needs. We offer a wide range of opportunities to sponsor programs, services, 
+          events, and activities that align with your brand and values.
+          <br><br>
+          We believe that everyone should have the right to make informed choices about their sexual health.
+          We’ve been working tirelessly for more than 50 years to protect and promote that right – because, for all
+          of the strides we’ve made, sexual and reproductive health rights continue to be undermined. How are we
+          closing the gap? We harness the power of technology to serve the needs of youth in relevant and
+          innovative ways. We conduct research to develop programming and services that serve underserved
+          communities. We give youth a voice. We amplify voices that might otherwise be silenced. We invite you to
+          add your voice to ours.
+          <br><br>
+          When you partner with PPT, you do so much more than underwrite our programs and services. You invest
+          in the direct and meaningful impact they have on the people and communities of Toronto.
+        </p>
+
+        <h3>
+          Granting Funders, Government and City: Give Your
+          <br>
+          Community a Voice – Partner with PPT
+        </h3>
+        <p>
+          As a funder, you are passionate about solving social issues that affect your community. So are we. At
+          PPT, we believe that everyone should have the right to make informed choices about their sexual health.
+          We’ve been working tirelessly for more than 50 years to protect and promote that right – because, for all
+          of the strides we’ve made, sexual and reproductive health rights continue to be undermined. How are we
+          closing the gap? We harness the power of technology to serve the needs of youth in relevant and
+          innovative ways. We conduct research to develop programming and services that serve underserved
+          communities. We give youth a voice. We amplify voices that might otherwise be silenced. We invite you to
+          add your voice to ours. 
+          <br><br>
+          When you partner with PPT, that’s exactly what you get – a partner. We work collaboratively with our
+          funders to create positive change in our community. We work hard, every day, to earn the trust of the
+          youth who use our services and participate in our programs. We’ll work every bit as hard to earn yours.
+          We’re accredited, soundly structured, and have a long history of working with funders to deliver on
+          objectives, and meet targets. We’re realistic about both. Of course, that alone isn’t what makes us a
+          smart investment. What does is our unwavering accountability – not only to you as our partner, but to the
+          youth we serve.
+        </p>
+      </section>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>

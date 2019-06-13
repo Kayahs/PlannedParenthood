b5379857c9f84cb4aee6fd10758583461015cca5
Kayahs <akshaykmanchanda@gmail.com>
2019-06-13 13:38:38 -0400

    changed title to get from wordpress and change links to open in new page instead of downloading

--- wp-content/themes/PPTTheme/page-annual-reports.php
+++ wp-content/themes/PPTTheme/page-annual-reports.php
@@ -11,7 +11,7 @@ get_header(); ?>
     <main id="main" class="site-main annual-reports" role="main">
 
       <header class="banner">
-        <h1>annual reports</h1>
+        <?php the_title( '<h1>', '</h1>' ); ?>
       </header>
 
       <section>
@@ -19,46 +19,46 @@ get_header(); ?>
         <iframe style="border:none;width:100%;height:500px;" src="//e.issuu.com/embed.html#28573712/62553054" allowfullscreen></iframe>
         
         <h4>download</h4>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_ANNUALREPORT_2018_WEB.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_ANNUALREPORT_2018_WEB.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto Annual Report 2017-2018</p>
         </a>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_ANNUALREPORT_2017.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_ANNUALREPORT_2017.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto Annual Report 2016-2017</p>
         </a>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_ANNUALREPORT_2016_WEB.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_ANNUALREPORT_2016_WEB.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto Annual Report 2015-2016</p>
         </a>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_2015_Web.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_2015_Web.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto Annual Report 2014-2015</p>
         </a>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_2014.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_2014.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto Annual Report 2013-2014</p>
         </a>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_2013_online.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_2013_online.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto Annual Report 2012-2013</p>
         </a>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_2012-FINAL.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_2012-FINAL.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto Annual Report 2011-2012</p>
         </a>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_10-11_pdf.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_10-11_pdf.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto Annual Report 2010-2011</p>
         </a>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_09-10.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/annual-report/PPT_AnnualReport_09-10.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto Annual Report 2009-2010</p>
         </a>
 
         <h3 class="strategic-h3">strategic plan</h3>
         <h4>download</h4>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/strategic-plan/Strategy_Map_Updated_2018_Final.pdf" download>
+        <a href="<?php bloginfo('template_directory');?>/assets/PDF/strategic-plan/Strategy_Map_Updated_2018_Final.pdf" target="_blank">
           <img src="<?php bloginfo('template_directory');?>/assets/icons/green-pdf.svg">
           <p>Planned Parenthood Toronto 2015-2020 Strategic Plan</p>
         </a>

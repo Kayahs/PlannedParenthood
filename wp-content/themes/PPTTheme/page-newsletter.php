ec7d7628ea62ce282719daefc8151e110e026e36
Kayahs <akshaykmanchanda@gmail.com>
2019-08-19 10:36:10 -0400

    Adjusted newsletter page to be editable

--- wp-content/themes/PPTTheme/page-newsletter.php
+++ wp-content/themes/PPTTheme/page-newsletter.php
@@ -14,25 +14,13 @@ get_header(); ?>
       </header>
 
       <section>
-        <h3>current issue</h3>
-        <iframe style="border:none;width:100%;height:500px;" src="//e.issuu.com/embed.html#28573712/66212691" allowfullscreen></iframe>
-
-        <h3 class="strategic-h3">sign up for more news!</h3>
 
         <?php while ( have_posts() ) : the_post(); ?>
 
           <?php the_content(); ?>
 
         <?php endwhile; ?>
-        <h3 class="strategic-h3">download a history of choice</h3> 
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/newsletter/Newsletter_web_format_2018.pdf" >
-          <img src="<?php bloginfo('template_directory');?>/assets/icons/pink-pdf.png">
-          <p>Planned Parenthood Toronto Newsletter 2018</p>
-        </a>
-        <a href="<?php bloginfo('template_directory');?>/assets/PDF/newsletter/Newsletter_Issue1-2017-FINAL.pdf" >
-          <img src="<?php bloginfo('template_directory');?>/assets/icons/pink-pdf.png">
-          <p>Planned Parenthood Toronto  Newsletter 2017</p>
-        </a>
+
       </section>
 
     </main><!-- #main -->
